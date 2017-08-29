19d2b06ed793c1fbf62734f10a408dc986aad8be
Yuanzhao Chen <cheny0l@kl-16000.local>
2017-08-29 09:51:27 +0300

    alter priority queue operations

--- algorithms/shortest-path.cc
+++ algorithms/shortest-path.cc
@@ -4,8 +4,8 @@
 #include <utility>
 #include <climits>
 
-// #define DIJKSTRA_ALGORITHM
-#define SPFA_ALGORITHM
+#define DIJKSTRA_ALGORITHM
+// #define SPFA_ALGORITHM
 
 using namespace std;
 
@@ -66,7 +66,7 @@ void construct(Graph *G) {
     (G->nodes).push_back(E);
 }
 
-void dijkstra(Graph *G, int source) {
+void shortest_path(Graph *G, int source) {
     vector<Node *>::iterator nodeit;
 
     for (nodeit = G->nodes.begin(); nodeit != G->nodes.end(); nodeit++) {
@@ -76,14 +76,11 @@ void dijkstra(Graph *G, int source) {
 
     (G->nodes)[source] -> d = 0;
     (G->nodes)[source] -> mask = 1;
-    for (vector<pair<Node*, int> >::iterator neighborsit = G->nodes[source]->neighbors.begin(); neighborsit != G->nodes[source]->neighbors.end(); neighborsit++) {
-        // cout << (*neighborsit).first->value << " " << (*neighborsit).second <<endl;
-        (*neighborsit).first->d = (*neighborsit).second;
-    }
 
 #ifdef DIJKSTRA_ALGORITHM
     // for Dijkstra algrithm
      priority_queue<Node *, vector<Node *>, compare_min_first> Q;
+
 #endif
 
 #ifdef SPFA_ALGORITHM
@@ -91,42 +88,41 @@ void dijkstra(Graph *G, int source) {
      queue<Node *> Q;
 #endif
 
-     int size = (G->nodes).size();
-     for (int i = 0; i < size; i++) {
-         if (i != source) {
-             Q.push(G->nodes[i]);
-         }
-     }
+    Q.push(G->nodes[source]);
 
      while (!Q.empty()) {
 
 #ifdef DIJKSTRA_ALGORITHM
          Node *u = Q.top();
+         Q.pop();
 #endif
 
 #ifdef SPFA_ALGORITHM
-        Node *u = Q.front();
-#endif
-
+         Node *u = Q.front();
          Q.pop();
          u->mask = 0;
-        //  cout << u->value << endl;
+#endif
+
+         cout << u->value << " : " << u->d << endl;
 
          for (vector<pair<Node*, int> >::iterator neighborsit = u->neighbors.begin(); neighborsit != u->neighbors.end(); neighborsit++) {
              if ((*neighborsit).first->d > u->d + (*neighborsit).second) {
                  (*neighborsit).first->d = u->d + (*neighborsit).second;
                  (*neighborsit).first->previous = u;
+                 cout << "  -->" << (*neighborsit).first->value << " : "<< (*neighborsit).first->d << endl;
 
                  if (!(*neighborsit).first->mask) {
                      Q.push((*neighborsit).first);
                      (*neighborsit).first->mask = 1;
                  }
+
              }
          }
      }
 }
 
 void print_node(Graph *G) {
+    cout << "\n" << "Final path length : " << "\n";
     for (vector<Node *>::iterator nodeit = G->nodes.begin(); nodeit != G->nodes.end(); nodeit++) {
         cout << (*nodeit)->value << " : " << (*nodeit)->d << "\n";
     }
@@ -135,7 +131,7 @@ void print_node(Graph *G) {
 int main(int argc, char const *argv[]) {
     Graph *G = new Graph();
     construct(G);
-    dijkstra(G, 0);
+    shortest_path(G, 0);
     print_node(G);
     return 0;
 }
